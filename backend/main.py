65e17c53991fc3a4b0a9457487c0fc48f77d2e5c
chrisHebs23 <christopher.heberden@gmail.com>
2026-04-13 20:15:52 -0500

    connected to supabase

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
+import os
 from fastapi import FastAPI
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {'status': 'Ok'}
+
+
+
+@app.get('/data')
+async def getData():
+    return supabase.table('tracks').select("*").execute()
