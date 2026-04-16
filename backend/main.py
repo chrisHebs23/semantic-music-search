799115aef0528d1b327b552ae441d6682693ff8d
chrisHebs23 <christopher.heberden@gmail.com>
2026-04-16 18:57:16 -0500

    Update cors issue

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://semantic-music-search.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
